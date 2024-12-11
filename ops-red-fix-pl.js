13460b8def641eb307a1b6cecfaa8713641dd550
Eduardo Roberto <euamotubaina@gmail.com>
2024-12-11 21:29:37 +0000

    Create ops-red-fix-pl.js

---
+++ ops-red-fix-pl.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name        OPS/RED: Fix PL
+// @namespace   https://github.com/euamotubaina
+// @version     2024-12-11-1
+// @description Fix PL links
+// @author      euamotubaina
+// @grant       none
+// @match       https://orpheus.network/torrents.php?id=*
+// @match       https://orpheus.network/artist.php?id=*
+// @match       https://redacted.sh/torrents.php?id=*
+// @match       https://redacted.sh/artist.php?id=*
+// @downloadURL https://raw.githubusercontent.com/euamotubaina/userscripts/main/ops-red-fix-pl.js
+// @updateURL   https://raw.githubusercontent.com/euamotubaina/userscripts/main/ops-red-fix-pl.js
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    Array.from(document.querySelectorAll('.torrent_row.group_torrent a[href^="torrents.php?id="]'))
+        .forEach(pl => {
+            if (pl.textContent === 'PL') pl.classList.remove("group_snatched", "gazelle_snatched", "gazelle_uploaded", "gazelle_leeching", "gazelle_seeding");
+            const m = pl.href.match(/id=\d+&torrentid=(\d+)(?:(#torrent\d+|.*?))$/);
+            if (m) {
+                pl.href = `torrents.php?torrentid=${m[1]}`;
+            }
+        });
+})();
