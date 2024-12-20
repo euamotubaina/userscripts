3ae1dd14fa149460862b8837e4bf438a87add520
Eduardo Roberto <euamotubaina@gmail.com>
2024-12-20 13:27:30 +0000

    ops-csshacks: Remove torrent group bg color

--- ops-csshacks.js
+++ ops-csshacks.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            OPS: CSS Hacks
 // @description     Collages below torrents, adds .tl_notice
-// @author          commoner@orpheus.network
+// @author          euamotubaina
 // @namespace       github.com/euamotubaina
-// @version         2024-12-19
+// @version         2024-12-20
 // @grant           none
-// @match           https://orpheus.network/torrents.php?id=*
+// @match           https://orpheus.network/torrents.php*
 // @match           https://orpheus.network/artist.php?id=*
 // @match           https://orpheus.network/collages.php?id=*
 // @downloadURL     https://raw.githubusercontent.com/euamotubaina/userscripts/main/ops-csshacks.js
@@ -17,6 +17,7 @@
 (function() {
     'use strict';
 
+    // collages below torrents
     const torrents = document.querySelector('#torrent_details, #discog_table');
     if (torrents) {
         Array.from(document.querySelectorAll('.collage_table')).reverse().forEach((collages) => {
@@ -27,17 +28,20 @@
 
     // css hacks
     // add .tl_notice class to fix the add_releases script
+    // darken torrent group headers background-color
     const tlNot = document.createElement('style');
-    tlNot.textContent = `
-        .tl_notice {
-            color: #d47744 !important;
-            font-weight: 400;
-        }
-        .main_column .td_info a[href^="http"]:hover,
-        .main_column .td_info a[href^="https"]:hover {
-            background: none !important;
-        }
-        `;
+    tlNot.textContent =
+`.tl_notice {
+    color: #d47744 !important;
+    font-weight: 400;
+}
+.main_column .td_info a[href^="http"]:hover,
+.main_column .td_info a[href^="https"]:hover {
+    background: none !important;
+}
+#torrent_details tr.edition {
+    background-color: transparent;
+}`;
     document.head.appendChild(tlNot);
 
 })();
